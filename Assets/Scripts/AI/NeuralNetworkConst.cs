9d6d277e0dab6bf7c59ebbed1cae61f05ca689ba
TheExE <gustmens12@gmail.com>
2016-05-05 15:08:20 +0300

    Added linear drag so that added force is not constant also increased ANN update steps for next generation

--- Assets/Scripts/AI/NeuralNetworkConst.cs
+++ Assets/Scripts/AI/NeuralNetworkConst.cs
@@ -30,6 +30,6 @@ public class NeuralNetworkConst
     public static int DEFENSE_NEURONS_PER_HID_LAY = 7;
 
     public static int TOURNAMENT_COMPETITIORS = 5;
-    public static int MAX_TICKS = 2000;
+    public static int MAX_TICKS = 3000;
     	
 }
